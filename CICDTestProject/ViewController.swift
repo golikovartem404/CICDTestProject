90ddbb0702c3b29bfc9d2432a073e6d42febea72
Artem Golikov <golikovartem404@gmail.com>
2024-05-03 07:23:58 +0200

    Initial Commit

---
+++ CICDTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTestProject
+//
+//  Created by Artem Golikov on 03.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
