a0b053a0df6f045764c81c67ff4d11d53a00ab49
Artem Golikov <golikovartem404@gmail.com>
2024-05-03 12:24:27 +0200

    Add label and change app name

--- CICDTestProject/ViewController.swift
+++ CICDTestProject/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    private lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to\nCI/CD Test Project"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 28, weight: .black)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupView() {
         view.backgroundColor = .systemMint
     }
-
-
+    
+    private func setupHierarchy() {
+        view.addSubview(welcomeLabel)
+    }
+    
+    private func setupLayout() {
+        welcomeLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.7)
+        }
+    }
 }
 
