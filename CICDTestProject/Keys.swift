56ba7512b23cb8fb9c1719205dc3e0e314805d2f
Artem Golikov <golikovartem404@gmail.com>
2024-05-06 07:22:57 +0200

    Set API keys

--- CICDTestProject/Keys.swift
+++ CICDTestProject/Keys.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Keys {
-    static let APIKey1 = ""
-    static let APIKey2 = ""
-    static let APIKey3 = ""
+    static let APIKey1 = "sjdkfh456kdnfkg44235"
+    static let APIKey2 = "asfk4389275gfb59091n"
+    static let APIKey3 = "57-abfgf-4785bbskhgdf-4682"
 }
